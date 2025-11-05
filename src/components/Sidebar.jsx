98fc7aea7a356941d34d182ce77f5c7069190800
Samrita-Swain <samritaswain28@gmail.com>
2025-11-05 12:49:53 +0530

    check

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -64,7 +64,7 @@ const Sidebar = ({ activeTab, setActiveTab }) => {
 
             className="flex justify-center"
           >
-            <img src="/images/logo.png" alt="logo" className="w-[50%]" />
+            <a href="/"><img src="/images/logo.png" alt="logo" className="w-[50%]" /></a>
 
        
             
